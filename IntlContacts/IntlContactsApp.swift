c53f9cfdab794348fd1a00bd74313312bf699761
Tyler Sheaffer <ty.sheaffer@gmail.com>
2022-03-06 09:33:15 -1000

    Initial Xcode project

---
+++ IntlContacts/IntlContactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntlContactsApp.swift
+//  IntlContacts
+//
+//  Created by Tyler Sheaffer on 3/6/22.
+//
+
+import SwiftUI
+
+@main
+struct IntlContactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
